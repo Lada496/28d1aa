50538e1a86747e2eb58ee4870b85fbbde63bcc69
Yuko Murayama <yukomurayama94@gmail.com>
2022-03-26 05:26:01 +0000

    fix messages order and latestMessageText

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -19,9 +19,9 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "ASC"]],
+      order: [[Message, "createdAt", "DESC"]],
       include: [
-        { model: Message},
+        { model: Message },
         {
           model: User,
           as: "user1",
@@ -50,7 +50,8 @@ router.get("/", async (req, res, next) => {
     for (let i = 0; i < conversations.length; i++) {
       const convo = conversations[i];
       const convoJSON = convo.toJSON();
-      
+      convoJSON.messages.sort((a,b)=>a.createdAt - b.createdAt)
+
       // set a property "otherUser" so that frontend will have easier access
       if (convoJSON.user1) {
         convoJSON.otherUser = convoJSON.user1;
@@ -68,7 +69,7 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length - 1].text;
       conversations[i] = convoJSON;
     }
 
